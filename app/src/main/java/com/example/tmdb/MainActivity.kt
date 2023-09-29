78f6d6b4bb7593da9a5f85f345e103867c5e6788
KSaid <kd.m.said@gmail.com>
2023-09-29 11:06:20 +0300

    adding project needed dependence

--- app/src/main/java/com/example/tmdb/MainActivity.kt
+++ app/src/main/java/com/example/tmdb/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.example.tmdb
 
-import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import dagger.hilt.android.AndroidEntryPoint
+import android.os.Bundle
 
-@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
 }
-
-
-
-
